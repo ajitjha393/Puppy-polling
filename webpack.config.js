4baefea1aabd0f191e0aa297084ae0220e3128e3
Bishwajit Jha <ajitjha393@gmail.com>
2021-09-14 16:50:17 +0530

    feat(app): added data streams

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'index.js',
+		path: __dirname + './dist',
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [{ test: /\.ts$/, use: 'awesome-typescript-loader' }],
+	},
+
+	devServer: {
+		port: 5555,
+	},
+}
